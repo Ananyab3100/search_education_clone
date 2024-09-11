7a1dcb14dc60cb34ca39a3d1d06e7f9ba382a8cc
Ananyab3100 <ananyabhagat2000@gmail.com>
2024-09-11 21:57:17 +0530

    Your descriptive message here

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 
 import './App.css';
-import SimpleSlider from './components/craousel';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import Searchbar from './components/Searchbar';
-import Card from './components/Card';
 import SearchUniversitySection from './components/SearchUniversitySection';
 import Footer from './components/Footer';
 
@@ -14,13 +11,11 @@ function App() {
     <div className="App ">
       <Navbar/>
       <Home/>
-      {/* <Searchbar/> */}
+     
       
       <SearchUniversitySection/>
       <Footer/>
-       {/* <div className="h-96">
-
-</div> */}
+     
       
     </div>
   );
